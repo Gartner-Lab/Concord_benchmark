edd1d84976c8e135dc28c831d5035f34c34a1d04
Qin Zhu <qinzhu@outlook.com>
2025-07-02 21:40:18 +0000

    ec2 update qz

---
+++ jobs/benchmark_cel_packerN2_hyperparam/benchmark_cel_packerN2_hyperparam_concord_hcl_concord_hcl_batch_size-1024.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+echo "Running on: $(hostname)"
+nvidia-smi --query-gpu=index,name,memory.total,driver_version --format=csv || true
+
+source ~/.bashrc
+conda activate cellpath
+
+TIMESTAMP=$(date +'%m%d-%H%M')
+python benchmark_cel_packerN2_hyperparam_concord_hcl_concord_hcl_batch_size-1024.py --timestamp $TIMESTAMP
---
+++ jobs/benchmark_cel_packerN2_hyperparam/benchmark_cel_packerN2_hyperparam_concord_hcl_concord_hcl_sampler_knn-1000.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+echo "Running on: $(hostname)"
+nvidia-smi --query-gpu=index,name,memory.total,driver_version --format=csv || true
+
+source ~/.bashrc
+conda activate cellpath
+
+TIMESTAMP=$(date +'%m%d-%H%M')
+python benchmark_cel_packerN2_hyperparam_concord_hcl_concord_hcl_sampler_knn-1000.py --timestamp $TIMESTAMP
