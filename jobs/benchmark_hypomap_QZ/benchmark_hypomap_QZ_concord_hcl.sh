61181a7c1746062a894e33dd1ca7ce92f7ebe0fc
Zhu <qinzhu@outlook.com>
2025-07-02 15:55:18 -0700

    local_qz_update_hypomap

---
+++ jobs/benchmark_hypomap_QZ/benchmark_hypomap_QZ_concord_hcl.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+echo "Running on: $(hostname)"
+nvidia-smi --query-gpu=index,name,memory.total,driver_version --format=csv || true
+
+source ~/.bashrc
+conda activate concord
+
+TIMESTAMP=$(date +'%m%d-%H%M')
+python benchmark_hypomap_QZ_concord_hcl.py --timestamp $TIMESTAMP
