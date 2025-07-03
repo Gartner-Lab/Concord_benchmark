f9d7c686e366e07a02b1f881f54dc2526427244d
ZuzhiLeoJiang <zuzhijiang.sci@gmail.com>
2025-07-03 15:50:29 -0700

    Clean up pycache files.

---
+++ jobs/submit_dkd_step2(_ext).sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+#$ -j y
+#$ -r y
+#$ -l mem_free=32G
+#$ -l scratch=50G
+#$ -l h_rt=03:00:00
+
+source ~/.bashrc
+conda activate scenv
+
+python ../notebook/benchmark_dkd_Wilson_step2_extend.py
+
